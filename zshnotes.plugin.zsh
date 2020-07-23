b655b191c04b72ebf28f9bf6528271197e3938f2
Jake Gearon <jake.gearon@gmail.com>
2020-07-22 19:58:11 -0500

    Update zshnotes.plugin.zsh
    changed default folder

--- zshnotes.plugin.zsh
+++ zshnotes.plugin.zsh
@@ -12,7 +12,7 @@ note_submit() {
     task=$(echo -E "$@" | tr '\n' '\000' | sed 's:\x00\x00.*:\n:g' | tr '\000' '\n')
 
     if [[ ! -e $day_path ]]; then
-        mkdir -p "$HOME/zshnotes"
+        mkdir -p "$HOME/Documents/zshnotes"
         touch $day_path
         chmod +x $day_path
         fold -w 80 -s $day_path
@@ -34,7 +34,7 @@ note_read() {
   local cur_day
   cur_day=$(date +"%m-%d-%y")
   local day_path
-  day_path="$HOME/zshnotes/"$cur_day".txt"
+  day_path="$HOME/Documents/zshnotes/"$cur_day".txt"
   if [[ ! -e $day_path ]]; then
         mkdir -p "$HOME/zshnotes"
         touch $day_path
